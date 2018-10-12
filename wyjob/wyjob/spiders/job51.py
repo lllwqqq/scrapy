2fe2b69265d71a4a225c23073b06926e2d477f1f
lllwqqq <lllwqqq@163.com>
2018-10-12 13:51:56 +0800

    init

--- wyjob/wyjob/spiders/job51.py
+++ wyjob/wyjob/spiders/job51.py
@@ -5,21 +5,15 @@ import scrapy
 class A51jobSpider(scrapy.Spider):
     name = '51job'
     allowed_domains = ['51job.com']
-    # start_urls = ['https://www.51job.com/']
 
-    def get_urls(self):
-        start_urls = ['https://www.51job.com/']
-        return scrapy.Request(start_urls,callback=self.get_categorys)
 
-    def get_categorys(self,response):
+    def start_requests(self):
+        start_urls = ('https://www.51job.com/')
+        yield scrapy.Request(start_urls,callback=self.parse)
+
+    def parse(self, response):
         categorys = response.xpath("//*[@class='cn hlist']//*[@class='e']/a/@href")
         for category in categorys:
             print (category.extract())
+            print ('-'*10+'>')
 
-    # def parse(self, response):
-    #     categorys = response.xpath("//*[@class='cn hlist']//*[@class='e']/a/@href")
-    #     for category in categorys:
-    #         categoryUrl = category
-
-
-    # def getJobs(self,response):
